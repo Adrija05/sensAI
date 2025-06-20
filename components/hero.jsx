60e97aba5cfd3eba57452211ca07cbed66170707
Anusha Burnwal <145285088+Anushaburnwal@users.noreply.github.com>
2025-06-21 03:54:13 +0530

    Photo Updated

--- components/hero.jsx
+++ components/hero.jsx
@@ -68,7 +68,7 @@ const HeroSection = () => {
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
-              src="/banner.jpeg"
+              src="/ai.jpg"
               width={1280}
               height={720}
               alt="Dashboard Preview"
